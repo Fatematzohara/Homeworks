372eec82c16f9c6e7894cacf0e87c6ab2d719760
fatematzohara <fatematuz.qa@gmail.com>
2024-03-09 11:01:22 -0500

    hw6

--- FullName.java
+++ FullName.java
@@ -1,10 +1,18 @@
-package hw5JavaMethods;
+package hw6JavaWrapperAndCasting;
 
 public class FullName {
 
-	public String fullName(String fName, String lName) {
+	public String fullName(int fName, int lName) {
+		String convertedName = (String.valueOf(fName) + String.valueOf(lName));// converted
 		System.out.println("Family Member: " + fName + " " + lName);
-		return "fullName";
+		return convertedName;
+	}
+
+	public int salary(String januarySalary, double februarySalary) {
+		System.out.println("Average Salary: " + (Integer.parseInt(januarySalary) + (int) (februarySalary)) / 2);// converted
+
+		return 0;
+
 	}
 
 	public static void main(String[] args) {
@@ -12,10 +20,11 @@ public class FullName {
 		FullName member = new FullName();
 
 		// I can call the method multiple times with different arguments
-		member.fullName("Fatematuz", "Zohara");
-		member.fullName("Silia", "khan");
-		member.fullName("Raihan", "Khan");
-		member.fullName("Sunny", "khan");
+		member.fullName(120, 23);
+		member.fullName(13, 24);
+		member.salary("123", 1.33);
+		member.salary("10", 20.33);
 
 	}
+
 }
--- MyFamily.java
+++ MyFamily.java
@@ -1,20 +1,21 @@
-package hw5JavaMethods;
+package hw6JavaWrapperAndCasting;
 
 public class MyFamily {
-	public int child1Age = 20; // public
+	public float child1Age = 20.00f; // public
 	protected int child2Age = 24; // protected
-	int child3Age = 25; // default
+	double child3Age = 25.40;
 	public int child4Age = 27; // public
 
 	public int myFamily() {
-		int sum = child1Age + child2Age + child3Age + child4Age;
-		System.out.println("The sum of my children's age is: " + sum);
+
+		int sum = (int) (child1Age + child2Age + child3Age + child4Age);// converting to integer
+		System.out.println("The sum of my childrens age is: " + sum);
 		return sum;
 	}
 
 	public static void main(String[] args) {
 		MyFamily family = new MyFamily();
-		 family.myFamily();
-		
+		family.myFamily();
+
 	}
 }
